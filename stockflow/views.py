487c2a2b41c556ba3b0204bc1f1d6ec8d810937c
AflaxCade <aflah.cade@gmail.com>
2024-06-08 17:11:09 +0300

    created login functionality for users to login

--- stockflow/views.py
+++ stockflow/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .decorators import unauthenticated_user
 
 # Create your views here.
 
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password =request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # get 'next' parameter from request
+            next_url = request.GET.get('next')
+            if next_url:  # if 'next' parameter exists, redirect to its value
+                return redirect(next_url)
+            else:  # if 'next' parameter does not exist, redirect to 'home'
+                return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    context = {}
+    return render(request, 'registration/login.html', context)
+
+
+
+
+
+@login_required(login_url='login')
 def home(request):
     return render(request, 'dashboard.html', {})
