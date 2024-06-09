528904ec26a635107ffe2ddcd6e64f121eb6a9eb
AflaxCade <aflah.cade@gmail.com>
2024-06-09 15:16:11 +0300

    added customer url path

--- stockflow/urls.py
+++ stockflow/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+
     path('', views.home, name='home'),
+    path('customer/', views.customer, name='customer'),
 ]
