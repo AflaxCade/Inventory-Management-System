fa87db8ac5d7b1d02a93bf54e291670325c8b43f
AflaxCade <aflah.cade@gmail.com>
2024-06-09 18:15:32 +0300

    imported the signal in the app.py

--- stockflow/apps.py
+++ stockflow/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StockflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'stockflow'
+
+    def ready(self):
+        import stockflow.signals
