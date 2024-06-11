7a25b37cc218a2233316eaa4c99ad05f49a54cee
AflaxCade <aflah.cade@gmail.com>
2024-06-11 14:26:07 +0300

    added supplier model

--- stockflow/models.py
+++ stockflow/models.py
@@ -13,3 +13,15 @@ class Customer(models.Model):
 	
 	def __str__(self):
 		return self.name
+
+class Supplier(models.Model):
+	name = models.CharField(max_length=200)
+	contac = models.CharField(max_length=200, blank=True, null=True)
+	email = models.EmailField(unique=True)
+	phone = models.CharField(max_length=30, blank=True, null=True)
+	address = models.TextField(blank=True, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
