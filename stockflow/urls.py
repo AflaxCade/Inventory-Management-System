52191f30536c129791474db9dedd1f9899a42f1b
AflaxCade <aflah.cade@gmail.com>
2024-06-12 11:34:32 +0300

    added delete supplier link

--- stockflow/urls.py
+++ stockflow/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('profile/', views.profile, name='profile'),
 
     path('', views.home, name='home'),
     path('customer/', views.customer, name='customer'),
-    path('profile/', views.profile, name='profile'),
+    path('supplier/', views.supplier, name='supplier'),
+    path('delete_supplier/<str:pk>/', views.deleteSupplier, name='delete_supplier'),
 ]
