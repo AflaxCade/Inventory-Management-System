bcd91b9e97460ad41ebec928af2e47175a0130e4
AflaxCade <aflah.cade@gmail.com>
2024-06-21 18:51:09 +0300

    added four reset password urls

--- stockflow/urls.py
+++ stockflow/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from .forms import CustomPasswordResetForm, CustomSetPasswordForm
 from . import views
 
 urlpatterns = [
@@ -13,4 +15,13 @@ urlpatterns = [
     path('supplier/', views.supplier, name='supplier'),
     path('create_supplier/', views.createSupplier, name='create_supplier'),
     path('delete_supplier/<str:pk>/', views.deleteSupplier, name='delete_supplier'),
+
+
+    # reset password urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="account/password_reset_form.html", form_class=CustomPasswordResetForm), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html", form_class=CustomSetPasswordForm),name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_complete.html"), name='password_reset_complete')
+
+
 ]
