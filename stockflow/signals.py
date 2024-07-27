3d482d1ce24939141fed5ef88d12465cd208d320
AflaxCade <aflah.cade@gmail.com>
2024-07-27 17:57:04 +0300

    Added try...except block to handle potential errors, Added a docstring to describe the purpose of the signal function and Added update_user_email Signal to update the User email when the Customer email is changed.

--- stockflow/signals.py
+++ stockflow/signals.py
@@ -1,18 +1,36 @@
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
+from django.dispatch import receiver
+from django.contrib.auth.models import User, Group
 from .models import Customer
 
 
+@receiver(post_save, sender=User)
 def customer_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create a Customer profile when a new User is created.
+    """
     if created:
-        group = Group.objects.get(name='customer')
-        instance.groups.add(group)
-        Customer.objects.create(
-            user=instance,
-            name=instance.username,
-            email=instance.email
-        )
-        print('Profile created!')
+        try:
+            group, _ = Group.objects.get_or_create(name='customer')
+            instance.groups.add(group)
+            Customer.objects.create(
+                user=instance,
+                name=instance.username,
+                email=instance.email
+            )
+            print(f'Customer profile created for user: {instance.username}')
+        except Group.DoesNotExist:
+            print('Group "customer" does not exist.')
+        except Exception as e:
+            print(f'Error creating customer profile: {e}')
 
-post_save.connect(customer_profile, sender=User)
+@receiver(post_save, sender=Customer)
+def update_user_email(sender, instance, **kwargs):
+    """
+    Signal to update the User email when the Customer email is updated.
+    """
+    user = instance.user
+    if user:
+        user.email = instance.email
+        user.save()
+        print(f'User email updated for user: {user.username}') 
