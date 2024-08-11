0282ec9255f83e828e481697ec3ee4a6b0214da3
AflaxCade <aflah.cade@gmail.com>
2024-08-11 14:16:57 +0300

    created Product model, migrated and added in the admin

--- stockflow/admin.py
+++ stockflow/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Supplier, Category
+from .models import Customer, Supplier, Category, Product
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Supplier)
 admin.site.register(Category)
+admin.site.register(Product)
--- stockflow/models.py
+++ stockflow/models.py
@@ -58,3 +58,20 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    """
+    Model to represent a Product.
+    """
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
