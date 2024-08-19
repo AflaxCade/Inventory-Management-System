4df66548c4657f75168f54030072bf0faa7bddaa
AflaxCade <aflah.cade@gmail.com>
2024-08-19 04:22:20 +0300

    Register Order model in Django admin

--- stockflow/admin.py
+++ stockflow/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Supplier, Category, Product
+from .models import Customer, Supplier, Category, Product, Order
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Supplier)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Order)
