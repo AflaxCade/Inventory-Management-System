b7f602da568f225d2c9c8f42bc3f6fd8084d6f30
AflaxCade <aflah.cade@gmail.com>
2024-09-03 17:48:53 +0300

    added default status Pending to order model

--- stockflow/models.py
+++ stockflow/models.py
@@ -92,7 +92,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    status = models.CharField(max_length=200, choices=STATUS)
+    status = models.CharField(max_length=200, choices=STATUS, default='Pending')
     date_created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
