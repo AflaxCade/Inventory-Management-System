f26c52b1bb06649fe9096d6545d4e3044297718f
AflaxCade <aflah.cade@gmail.com>
2024-10-07 03:33:00 +0300

    added reset password email template

--- stockflow/urls.py
+++ stockflow/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .forms import CustomPasswordResetForm, CustomSetPasswordForm, CustomPasswordChangeForm
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('profile/', views.profile, name='profile'),
+
+    path('email_template/', TemplateView.as_view(template_name='account/email_template.html'), name='email_template'),
 
     path('', views.home, name='home'),
     path('user/', views.userPage, name='user_page'),
@@ -42,7 +45,7 @@ urlpatterns = [
     path('invoice_details/<str:pk>/', views.invoiceDetails, name='invoice_details'),
 
     # reset password urls
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="account/password_reset_form.html", form_class=CustomPasswordResetForm), name='password_reset'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="account/password_reset_form.html", html_email_template_name='account/email_template.html', form_class=CustomPasswordResetForm), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html", form_class=CustomSetPasswordForm),name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_complete.html"), name='password_reset_complete'),
