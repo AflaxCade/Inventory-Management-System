f18adae1adb7929943bc823bc5e1224c32987968
AflaxCade <aflah.cade@gmail.com>
2024-12-23 09:11:52 +0300

    resize the profile picture upon uploading

--- stockflow/models.py
+++ stockflow/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from PIL import Image
 
 
 class Customer(models.Model):
@@ -17,6 +18,16 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.name if self.name else "Unnamed Customer"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        if self.profile_pic:
+            img = Image.open(self.profile_pic.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.profile_pic.path)
 
 
 class Supplier(models.Model):
